27b778c43f33975ae2faa4195e333d523e6e0a10
coreylu27 <27luc@nsboroschools.net>
2025-02-12 14:44:04 -0500

    split word functions into own file

--- generate_letters.py
+++ generate_letters.py
@@ -1,29 +1,11 @@
 import json
 import string
 import random
-
-# Opening JSON file
-with open('dictionary.json') as json_file:
-    words = set(json.load(json_file).keys())  # Ensure words are a set for faster lookup
+import words as w
 
 letters = list(string.ascii_lowercase)
 
-def isPangram(word: str, game_letters: set) -> bool:
-    word_letters = set(word)
-    return word_letters == game_letters
-
-def getScore(word: str, letters: set, center_letter: str):
-    pangram = isPangram(word, letters)
-    word_letters = set(word)
-    if (
-        word in words and
-        center_letter in word and
-        len(word) >= 4 and
-        word_letters.issubset(letters)
-    ):
-        score = 1 if len(word) == 4 else len(word)
-        return score, pangram
-    return 0, False
+word_list = w.words_list
 
 def checkValidGame(game_letters: set, center_letter: str) -> bool:
     max_score, pangram = getMaxScore(game_letters, center_letter)
@@ -43,8 +25,8 @@ def generate_random_game():
 def getMaxScore(game_letters: set, center_letter: str):
     score = 0
     pangram_found = False
-    for word in words:
-        word_score, is_pangram = getScore(word, game_letters, center_letter)
+    for word in word_list:
+        word_score, is_pangram = w.getScore(word, game_letters, center_letter)
         score += word_score
         if is_pangram:
             pangram_found = True
@@ -52,15 +34,15 @@ def getMaxScore(game_letters: set, center_letter: str):
 
 def printWords(game_letters: set, center_letter: str):
     valid_words = []
-    for word in words:
-        score, _ = getScore(word, game_letters, center_letter)
+    for word in word_list:
+        score, _ = w.getScore(word, game_letters, center_letter)
         if score > 0:
             valid_words.append((word, score))
 
     sorted_words = sorted(valid_words, key=lambda x: x[1], reverse=True)
     for word, score in sorted_words:
         print(f"Word: {word} Score: {score}")
-        print("Pangram: " + str(isPangram(word, game_letters)))
+        print("Pangram: " + str(w.isPangram(word, game_letters)))
 
 def find_one_valid_game(points):
     global minimum_points
---
+++ words.py
@@ -0,0 +1,24 @@
+import json
+import string
+
+with open('dictionary.json') as json_file:
+    words_list = set(json.load(json_file).keys())  # Ensure words are a set for faster lookup
+
+letters = list(string.ascii_lowercase)
+
+def isPangram(word: str, game_letters: set) -> bool:
+    word_letters = set(word)
+    return word_letters == game_letters
+
+def getScore(word: str, letters: set, center_letter: str):
+    pangram = isPangram(word, letters)
+    word_letters = set(word)
+    if (
+        word in words_list and
+        center_letter in word and
+        len(word) >= 4 and
+        word_letters.issubset(letters)
+    ):
+        score = 1 if len(word) == 4 else len(word)
+        return score, pangram
+    return 0, False
